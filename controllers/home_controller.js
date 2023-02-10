8f8c9af9905959c37f0f4e4963f58c383c07de7a
Rohit <rohitrajvns2020@gmail.com>
2023-02-11 01:08:49 +0530

    displayed user name with posts, and modified page styling

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,10 +3,18 @@ const Post = require('../models/post');
 module.exports.home = function(req, res){
     // return res.end('<h1>EXPRESS is ready for CODEIAL</h1>');
     // console.log(req.cookies);
-    Post.find({}, function(err, posts){
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // Populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
         return res.render('home', {
-            title: "Codeial | Home",
+            title: "Home", 
             posts: posts
-        });
-    });
+        })
+    })
 }
