548c13258e13aeb08bd11e31f524aa8cb0d0060d
Rohit <rohitrajvns2020@gmail.com>
2023-03-04 00:20:59 +0530

    Now user can delete their post and comment. Also improved SCSS of home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,7 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
-module.exports.home = function(req, res){
+module.exports.home = function (req, res) {
     // return res.end('<h1>EXPRESS is ready for CODEIAL</h1>');
     // console.log(req.cookies);
     // Post.find({}, function(err, posts){
@@ -12,17 +13,20 @@ module.exports.home = function(req, res){
 
     // Populate the user of each post
     Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        return res.render('home', {
-            title: "Home", 
-            posts: posts
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         })
-    })
+        .exec(function (err, posts) {
+            User.find({}, function (err, users) {
+                return res.render('home', {
+                    title: "Home",
+                    posts: posts,
+                    all_user: users
+                });
+            });
+        });
 }
