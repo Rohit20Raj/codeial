e989927528ea6135fea6b97c858221dc94613939
Rohit <rohitrajvns2020@gmail.com>
2023-03-09 23:03:27 +0530

    File upload

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 module.exports.profile = function(req, res){
     // res.end('<h1>User Profile</h1>');
@@ -11,12 +13,38 @@ module.exports.profile = function(req, res){
     });
 }
 
-module.exports.update = function(req, res){
+module.exports.update = async function(req, res){
+    // if(req.user.id == req.params.id){
+    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+    //         return res.redirect('back');
+    //     });
+    // }else{
+    //     return res.status(401).send('Unauthorized');
+    // }
+ 
     if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+        try{
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function(err){
+                if(err){console.log('***Multer Error: ', err);}
+
+                // console.log(req.file);
+                user.name = req.body.name;
+                user.email = req.body.email;
+                
+                if(req.file){
+                    if(user.avatar){
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            });
+        } catch(err){
             return res.redirect('back');
-        });
-    }else{
+        }
+    } else{
         return res.status(401).send('Unauthorized');
     }
 }
