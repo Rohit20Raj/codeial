5d673d4ae80e1844a21d9b47b973dc51e6181bc5
Rohit <rohitrajvns2020@gmail.com>
2023-03-15 18:52:12 +0530

    added Google Authentication

--- routes/user.js
+++ routes/user.js
@@ -14,9 +14,12 @@ router.post('/create', userController.create);
 
 //use passport as a middleware to authenticate
 router.post('/create-session', passport.authenticate('local', {
-    failureRedirect: '/users/sign-in'
+    failureRedirect: '/user/sign-in'
 }), userController.createSession)
 
 router.get('/sign-out', userController.destroySession);
+
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/user/sign-in'}), userController.createSession);
 
 module.exports = router;
